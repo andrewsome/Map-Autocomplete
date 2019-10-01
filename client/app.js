ab6f3d748fc7af891eaae4b19f345d371160dae9
Andrew <andrewzhu2012@gmail.com>
2019-10-02 09:40:51 +1000

    refactory

--- client/app.js
+++ client/app.js
@@ -1,50 +1,53 @@
+(function() {
+  const autoInput = document.getElementById("autocomplete-input");
 
-(function () {
-    const autoInput = document.getElementById('autocomplete-input');
-
-    autoInput.addEventListener("keyup", (ele) => {
-        // remoteCall(ele.target.value);
-        // throttle(remoteCall, 300)(ele.target.value);
-        debounce(remoteCall, 300)(ele.target.value);
-    })
-    const remoteCall = async (input) => {
-        let response = await fetch(`/api/autocomplete/${input}`);
-        let data = await response.json();
-        let content = document.getElementById('dropdownContent');
-        content.innerHTML = '';
-        // if (!input) {
-        //     document.querySelector('is-loading').innerHTML.style.display = 'none';
-        // }
-        if (!data.suggestions) {
-            return;
-        }
-        data.suggestions.forEach(item => {
-            content.insertAdjacentHTML('beforeend', `<a href="#" class="dropdown-item"> ${item.label}</a>`);
-
-        })
-        console.log(data);
+  autoInput.addEventListener("keyup", keyPressed => {
+    // remoteCall(ele.target.value);
+    // throttle(remoteCall, 300)(ele.target.value);
+    debounce(remoteCall, 300)(keyPressed.target.value);
+  });
+  const remoteCall = async input => {
+    let content = document.getElementById("dropdownContent");
+    content.innerHTML = "";
+    let triggerDrop = document.getElementById("dropdown-menu");
+    triggerDrop.classList.add("is-hidden");
+    const response = await fetch(`/api/autocomplete/${input}`);
+    const data = await response.json();
+    const control = document.getElementById("control");
+    control.classList.remove("is-loading");
+    if (!data.suggestions) {
+      triggerDrop.classList.add("is-hidden");
+      return;
     }
-
-}());
+    data.suggestions.forEach(item => {
+      triggerDrop.classList.remove("is-hidden");
+      content.insertAdjacentHTML(
+        "beforeend",
+        `<a href="#" class="dropdown-item"> ${item.label}</a>`
+      );
+    });
+    console.log(data);
+  };
+})();
 let inDebounce;
 let inThrottle;
 
 const debounce = (func, delay) => {
-    return function () {
-        const context = this
-        const args = arguments
-        clearTimeout(inDebounce)
-        inDebounce = setTimeout(() => func.apply(context, args), delay)
-    }
-}
+  return function() {
+    const context = this;
+    const args = arguments;
+    clearTimeout(inDebounce);
+    inDebounce = setTimeout(() => func.apply(context, args), delay);
+  };
+};
 const throttle = (func, limit) => {
-    return function () {
-        const args = arguments
-        const context = this
-        if (!inThrottle) {
-            func.apply(context, args)
-            inThrottle = true
-            setTimeout(() => inThrottle = false, limit)
-        }
+  return function() {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
     }
-}
+  };
+};
