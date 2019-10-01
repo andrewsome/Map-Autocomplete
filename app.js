591d21b8323c5fc21fb6bf122443128be3f174f3
Andrew <andrewzhu2012@gmail.com>
2019-10-02 09:07:55 +1000

    remove wrong files

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const rtouer = require('./routes/router');
-require('dotenv').config();
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(router);
-
-app.listen(PORT, () => console.log('Express server is running'));
