572c892b1c7b753f046707f8a3a1f8347e27a7b3
Andrew <andrewzhu2012@gmail.com>
2019-10-02 10:16:06 +1000

    minor bug fixed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const rtouer = require('./routes/router');
+const router = require('./routes/router');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
