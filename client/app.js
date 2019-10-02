9ffb17dd51c144178e415dcff3de84037ca0692e
Andrew <andrewzhu2012@gmail.com>
2019-10-02 10:06:09 +1000

    minor bug fixed

--- client/app.js
+++ client/app.js
@@ -1,20 +1,24 @@
-(function() {
-  const autoInput = document.getElementById("autocomplete-input");
+const autoInput = document.getElementById("autocomplete-input");
+let content = document.getElementById("dropdownContent");
+let triggerDrop = document.getElementById("dropdown-menu");
+const control = document.getElementById("control");
 
+(function() {
   autoInput.addEventListener("keyup", keyPressed => {
     // remoteCall(ele.target.value);
     // throttle(remoteCall, 300)(ele.target.value);
+    control.classList.add("is-loading");
     debounce(remoteCall, 300)(keyPressed.target.value);
   });
   const remoteCall = async input => {
-    let content = document.getElementById("dropdownContent");
     content.innerHTML = "";
-    let triggerDrop = document.getElementById("dropdown-menu");
     triggerDrop.classList.add("is-hidden");
+
     const response = await fetch(`/api/autocomplete/${input}`);
     const data = await response.json();
-    const control = document.getElementById("control");
+
     control.classList.remove("is-loading");
+
     if (!data.suggestions) {
       triggerDrop.classList.add("is-hidden");
       return;
@@ -23,31 +27,36 @@
       triggerDrop.classList.remove("is-hidden");
       content.insertAdjacentHTML(
         "beforeend",
-        `<a href="#" class="dropdown-item"> ${item.label}</a>`
+        `<a href="#" onClick="setInputValue(this)" class="dropdown-item"> ${item.label}</a>`
       );
     });
     console.log(data);
   };
-})();
-let inDebounce;
-let inThrottle;
+  let inDebounce;
+  let inThrottle;
 
-const debounce = (func, delay) => {
-  return function() {
-    const context = this;
-    const args = arguments;
-    clearTimeout(inDebounce);
-    inDebounce = setTimeout(() => func.apply(context, args), delay);
+  const debounce = (func, delay) => {
+    return function() {
+      const context = this;
+      const args = arguments;
+      clearTimeout(inDebounce);
+      inDebounce = setTimeout(() => func.apply(context, args), delay);
+    };
   };
-};
-const throttle = (func, limit) => {
-  return function() {
-    const args = arguments;
-    const context = this;
-    if (!inThrottle) {
-      func.apply(context, args);
-      inThrottle = true;
-      setTimeout(() => (inThrottle = false), limit);
-    }
+  const throttle = (func, limit) => {
+    return function() {
+      const args = arguments;
+      const context = this;
+      if (!inThrottle) {
+        func.apply(context, args);
+        inThrottle = true;
+        setTimeout(() => (inThrottle = false), limit);
+      }
+    };
   };
-};
+})();
+function setInputValue(keyPressed) {
+  autoInput.value = ele.innerText;
+  content.innerHTML = "";
+  triggerDrop.classList.add("is-hidden");
+}
